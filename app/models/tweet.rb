bb751ef6d897dfb0232e1c7e430beda986dd7b51
Devin Austin <devin.austin@gmail.com>
2012-04-18 11:23:16 -0700

    added a bit of styling and root controller

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,13 @@
 class Tweet < ActiveRecord::Base
   attr_accessible :author, :body, :tweeted_on, :tweetid
+  include Tire::Model::Search
+  include Tire::Model::Callbacks
+
+  mapping do
+    indexes :id,         :index    => :not_analyzed
+    indexes :body,       :analyzer => 'snowball', :boost      => 100
+    indexes :author,     :analyzer => 'keyword'
+    indexes :tweeted_on, :type     => 'date', :include_in_all => false
+    indexes :tweetid,    :index    => :not_analyzed
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Twoogle::Application.routes.draw do
+
+  root :to => 'root#index'
   resources :tweets
 
   # The priority is based upon order of creation:
@@ -50,7 +52,6 @@ Twoogle::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
