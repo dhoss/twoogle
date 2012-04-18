f0efeeeeee1101cf6b7f9346e48c537fcdc5bb3e
Devin Austin <devin.austin@gmail.com>
2012-04-18 10:16:30 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twoogle::Application.config.secret_token = '5347899f3db8fd6dd3a5203a15a8b963dc695e094be9ba33ce7ee74287dc82807c27f5aa55a98f584e3b75d2c03ffe049286e6d941137692f069bd632b5e3040'
